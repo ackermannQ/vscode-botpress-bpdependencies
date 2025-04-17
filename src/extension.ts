fa916d60fbe44641785a5d2df851fd9f709e1b8a
ackermannQ <ackermann.quentin@gmail.com>
2025-04-17 07:48:46 -0400

    Renaming extension properties "extension" to "botpress-tools"
    It makes it easier to find it among other dependencies when used in a real life vscode (not dev mode)

--- src/extension.ts
+++ src/extension.ts
@@ -7,49 +7,54 @@ import { openAi } from "./openAi";
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.commands.registerCommand("extension.addBpDependency", async () => {
-      const workspaceRoot = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
-      const activeEditor = vscode.window.activeTextEditor;
-      if (!activeEditor) {
-        vscode.window.showErrorMessage("No active editor found.");
-        return;
-      }
+    vscode.commands.registerCommand(
+      "botpress-tools.addBpDependency",
+      async () => {
+        const workspaceRoot = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+        const activeEditor = vscode.window.activeTextEditor;
+        if (!activeEditor) {
+          vscode.window.showErrorMessage("No active editor found.");
+          return;
+        }
 
-      let packageJsonPath = activeEditor.document.uri.fsPath;
-      const packageJsonfolderPath = path.dirname(packageJsonPath);
+        let packageJsonPath = activeEditor.document.uri.fsPath;
+        const packageJsonfolderPath = path.dirname(packageJsonPath);
 
-      if (!workspaceRoot) return;
+        if (!workspaceRoot) return;
 
-      packageJsonPath = path.join(packageJsonfolderPath, "package.json");
-      const interfacesPath = path.resolve(workspaceRoot, "interfaces");
-      if (!fs.existsSync(interfacesPath)) {
-        vscode.window.showErrorMessage(
-          "Interfaces folder not found at " + interfacesPath
+        packageJsonPath = path.join(packageJsonfolderPath, "package.json");
+        const interfacesPath = path.resolve(workspaceRoot, "interfaces");
+        if (!fs.existsSync(interfacesPath)) {
+          vscode.window.showErrorMessage(
+            "Interfaces folder not found at " + interfacesPath
+          );
+          return;
+        }
+
+        const options = fs
+          .readdirSync(interfacesPath)
+          .filter((f) =>
+            fs.lstatSync(path.join(interfacesPath, f)).isDirectory()
+          );
+
+        const selected = await vscode.window.showQuickPick(options, {
+          placeHolder: "Select an interface to add",
+        });
+        if (!selected) return;
+
+        const packageJson = JSON.parse(
+          fs.readFileSync(packageJsonPath, "utf-8")
         );
-        return;
+        if (!packageJson.bpDependencies) packageJson.bpDependencies = {};
+        packageJson.bpDependencies[selected] = `../../interfaces/${selected}`;
+
+        fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+        vscode.window.showInformationMessage(`Added bpDependency: ${selected}`);
       }
-
-      const options = fs
-        .readdirSync(interfacesPath)
-        .filter((f) =>
-          fs.lstatSync(path.join(interfacesPath, f)).isDirectory()
-        );
-
-      const selected = await vscode.window.showQuickPick(options, {
-        placeHolder: "Select an interface to add",
-      });
-      if (!selected) return;
-
-      const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
-      if (!packageJson.bpDependencies) packageJson.bpDependencies = {};
-      packageJson.bpDependencies[selected] = `../../interfaces/${selected}`;
-
-      fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-      vscode.window.showInformationMessage(`Added bpDependency: ${selected}`);
-    }),
+    ),
 
     vscode.commands.registerCommand(
-      "extension.removeBpDependency",
+      "botpress-tools.removeBpDependency",
       async () => {
         const workspaceRoot = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
         const activeEditor = vscode.window.activeTextEditor;
@@ -81,54 +86,61 @@ export function activate(context: vscode.ExtensionContext) {
       }
     ),
 
-    vscode.commands.registerCommand("extension.addBuildScript", async () => {
-      const workspaceRoot = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
-      const activeEditor = vscode.window.activeTextEditor;
-      if (!activeEditor) {
-        vscode.window.showErrorMessage("No active editor found.");
-        return;
+    vscode.commands.registerCommand(
+      "botpress-tools.addBuildScript",
+      async () => {
+        const workspaceRoot = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+        const activeEditor = vscode.window.activeTextEditor;
+        if (!activeEditor) {
+          vscode.window.showErrorMessage("No active editor found.");
+          return;
+        }
+
+        let packageJsonPath = activeEditor.document.uri.fsPath;
+        const packageJsonfolderPath = path.dirname(packageJsonPath);
+
+        if (!workspaceRoot) return;
+        packageJsonPath = path.join(packageJsonfolderPath, "package.json");
+
+        const packageJson = JSON.parse(
+          fs.readFileSync(packageJsonPath, "utf-8")
+        );
+        if (!packageJson.scripts.build) packageJson.scripts.build = "";
+
+        if (packageJson.scripts.build === "bp add -y && bp build") {
+          vscode.window.showErrorMessage(
+            "Botpress build script already exists"
+          );
+          return;
+        }
+
+        packageJson.scripts.build =
+          packageJson.scripts.build + " && bp add -y && bp build";
+        fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+        vscode.window.showInformationMessage("Added build script");
       }
+    ),
 
-      let packageJsonPath = activeEditor.document.uri.fsPath;
-      const packageJsonfolderPath = path.dirname(packageJsonPath);
-
-      if (!workspaceRoot) return;
-      packageJsonPath = path.join(packageJsonfolderPath, "package.json");
-
-      const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
-      if (!packageJson.scripts.build) packageJson.scripts.build = "";
-
-      if (packageJson.scripts.build === "bp add -y && bp build") {
-        vscode.window.showErrorMessage("Botpress build script already exists");
-        return;
-      }
-
-      packageJson.scripts.build =
-        packageJson.scripts.build + " && bp add -y && bp build";
-      fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-      vscode.window.showInformationMessage("Added build script");
-    }),
-
-    vscode.commands.registerCommand("extension.createIntegration", () => {
+    vscode.commands.registerCommand("botpress-tools.createIntegration", () => {
       const terminal = vscode.window.createTerminal(`Botpress Init`);
       terminal.sendText(`bp init --workDir integrations --type integration`);
       terminal.show();
     }),
 
-    vscode.commands.registerCommand("extension.createBot", () => {
+    vscode.commands.registerCommand("botpress-tools.createBot", () => {
       const terminal = vscode.window.createTerminal(`Botpress Init`);
       terminal.sendText(`bp init --workDir bots --type bot`);
       terminal.show();
     }),
 
-    vscode.commands.registerCommand("extension.createPlugin", () => {
+    vscode.commands.registerCommand("botpress-tools.createPlugin", () => {
       const terminal = vscode.window.createTerminal(`Botpress Init`);
       terminal.sendText(`bp init --workDir plugins --type plugin`);
       terminal.show();
     }),
 
     vscode.commands.registerCommand(
-      "extension.generateHubFile",
+      "botpress-tools.generateHubFile",
       async (uri: vscode.Uri) => {
         if (uri && uri.fsPath.endsWith("hub.md")) {
           const activeEditor = vscode.window.activeTextEditor;
